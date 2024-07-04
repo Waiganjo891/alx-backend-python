c4f96de4549ec58d55e3c372798e8498476cb2ed
Your Name <you@example.com>
2024-07-04 03:25:15 -0700

    Task 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for adding two floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
