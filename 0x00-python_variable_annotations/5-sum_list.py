439b7684c03971e8048883307466a658341d6dca
Your Name <you@example.com>
2024-07-04 08:48:31 -0700

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Sums a list of floats.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sums a list of floats.
+    Args:
+        input_list (List[float]): A list of floats.
+    Returns:
+        float: The sum of the list of floats.
+    """
+    return sum(input_list)
