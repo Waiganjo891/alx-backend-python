d80eab72b2ca6f6d4e4027e2a8d2a6ecea29c863
Your Name <you@example.com>
2024-07-04 08:32:34 -0700

    Task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module 4-define_variables
+This module defines and annotates four variables:
+- a: an integer with a value of 1
+- pi: a float with a value of 3.14
+- i_understand_annotations: a boolean with a value of True
+- school: a string with a value of "Holberton"
+"""
+
+a: int = 1
+"""int: an integer with a value of 1"""
+
+pi: float = 3.14
+"""float: a float with a value of 3.14"""
+
+i_understand_annotations: bool = True
+"""bool: a boolean with a value of True"""
+
+school: str = "Holberton"
+"""str: a string with a value of 'Holberton'"""
