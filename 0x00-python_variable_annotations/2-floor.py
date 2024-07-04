0b240c179e3a0ad48e8b1e92cb8be59b07ea74e1
Your Name <you@example.com>
2024-07-04 07:29:54 -0700

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module 2-floor
+This module provides a function to compute the floor of a given float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the given float.
+    Parameters:
+    n (float): The float to compute the floor of.
+    Returns:
+    int: The floor of the float.
+    """
+    return math.floor(n)
