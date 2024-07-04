d5a25a041e060c4ecb32596040c9bddd4db828e4
Your Name <you@example.com>
2024-07-04 09:28:41 -0700

    Task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,30 +2,25 @@
 """
 This module provides a function to create a multiplier function.
 """
-
 from typing import Callable
+
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Creates a multiplier function.
-    
     Args:
         multiplier (float): The multiplier value.
-    
     Returns:
-        Callable[[float], float]: A function that multiplies a float by the given multiplier.
+        Callable[[float], float]: A function that multiplies
+        a float by the given multiplier.
     """
-    
     def multiplier_func(x: float) -> float:
         """
         Multiplies a float by the outer multiplier.
-        
         Args:
             x (float): The value to be multiplied.
-        
         Returns:
             float: The result of the multiplication.
         """
         return x * multiplier
-    
     return multiplier_func
