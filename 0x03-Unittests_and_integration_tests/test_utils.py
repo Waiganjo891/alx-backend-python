f0dbf18d937ed150accf8516cdd2d6190de3a74d
Your Name <you@example.com>
2024-07-29 04:58:02 -0700

    Task 00

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,8 +9,7 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-    A TestAccessNestedMap class that inherits
-    from unittest.TestCase
+    Test cases for access_nested_map function.
     """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -19,41 +18,9 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """
-        Test access_nested_map with valid inputs
+        Test access_nested_map with different nested maps and paths.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
-    ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        """
-        Test access_nested_map to raise KeyError
-        with invalid inputs
-        """
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), str(path[-1]))
-
-    @patch('utils.requests.get')
-    def test_get_json(self, mock_get):
-        """
-        Test get_json returns the expected
-        result with mocked requests.get.
-        """
-        test_cases = [
-            ("http://example.com", {"payload": True}),
-            ("http://holberton.io", {"payload": False}),
-        ]
-        for test_url, test_payload in test_cases:
-            mock_response = Mock()
-            mock_response.json.return_value = test_payload
-            mock_get.return_value = mock_response
-            result = get_json(test_url)
-            mock_get.assert_called_once_with(test_url)
-            self.assertEqual(result, test_payload)
-            mock_get.reset_mock()
 
 
 if __name__ == "__main__":
