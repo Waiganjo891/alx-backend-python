aadc0ebd126e6c5e9f60ab68a8dd99b4dfe7cfed
Your Name <you@example.com>
2024-07-29 02:45:44 -0700

    Tasks

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,20 +1,75 @@
 #!/usr/bin/env python3
+"""Generic utilities for github org client.
 """
-This module provides utility functions for accessing nested maps.
-"""
+import requests
+from functools import wraps
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+__all__ = [
+    "access_nested_map",
+    "get_json",
+    "memoize",
+]
 
 
-def access_nested_map(nested_map, path):
-    """
-    Access a nested object in `nested_map` with the sequence of keys in `path`.
-    Args:
-        nested_map (dict): A dictionary to access.
-        path (tuple): A sequence of keys to access in the dictionary.
-    Returns:
-        The value found by traversing the nested_map with the given path.
-    Raises:
-        KeyError: If a key in the path is not found in the nested_map.
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
+    """Access nested map with key path.
+    Parameters
+    ----------
+    nested_map: Mapping
+        A nested map
+    path: Sequence
+        a sequence of key representing a path to the value
+    Example
+    -------
+    >>> nested_map = {"a": {"b": {"c": 1}}}
+    >>> access_nested_map(nested_map, ["a", "b", "c"])
+    1
     """
     for key in path:
+        if not isinstance(nested_map, Mapping):
+            raise KeyError(key)
         nested_map = nested_map[key]
+
     return nested_map
+
+
+def get_json(url: str) -> Dict:
+    """Get JSON from remote URL.
+    """
+    response = requests.get(url)
+    return response.json()
+
+
+def memoize(fn: Callable) -> Callable:
+    """Decorator to memoize a method.
+    Example
+    -------
+    class MyClass:
+        @memoize
+        def a_method(self):
+            print("a_method called")
+            return 42
+    >>> my_object = MyClass()
+    >>> my_object.a_method
+    a_method called
+    42
+    >>> my_object.a_method
+    42
+    """
+    attr_name = "_{}".format(fn.__name__)
+
+    @wraps(fn)
+    def memoized(self):
+        """"memoized wraps"""
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return property(memoized)
